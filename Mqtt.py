7b82a14da023da4fb855ddc409ed1a4c6dcb9f3f
Gabriel Oliveira Aragão <31046857+linknasubi@users.noreply.github.com>
2019-08-20 12:56:57 -0300

    Update Mqtt.py

--- Mqtt.py
+++ Mqtt.py
@@ -2,33 +2,54 @@ import paho.mqtt.client as mqtt
 import time
 import json
 
-def getJSON(filePathAndName):
-    with open(filePathAndName, 'r') as fp:
-        return json.load(fp)
+class Reclosers:
     
-def Gener(num):
-    clientes = []
-    for i in range (num):
-        clientes.append("cliente"+str(i))
-        clientes[i] = mqtt.Client(str(i))
-    return clientes
 
-clientes = Gener(5)
-
-def Rep(clientes):
-    a = -1
-    while True:
-        for _ in clientes:
-            myObj = getJSON('./Teste3.json')
-            recloser = myObj.get("Recloser")
-            a += 1
-            obj = recloser[a]
-            _.connect("127.0.0.1")
-            _.loop_start()
-            _.publish("Topic","Message:"+str(obj)+" From Client "+str(a))
-            _.loop_stop()
-        a = -1
-        time.sleep(10)
+#    Reclosers que estão enviando seus estados a partir da leitura de um arquivo json.
+#    Estes devem armazenar seus estados atuais em um arquivo json gerado pelo próprio código. 
+    
+    def __init__(self):
+        self.num = 18
         
-Teste = Rep(clientes)
+    def createJSON(self):
+        with open ("Teste4.json", "w") as outfile:
+            List = []
+            for _ in range (self.num):
+                Recloser = ([{"Id": str(_), "Chave": False, "Voltage": 0, "state": False, "waiting_state_change": False, "waiting_voltage_change": False}])
+                List.append(Recloser)
+            json.dump(List, outfile)
+
+    def getJSON(self):
+        with open("Teste4.json", 'r') as fp:
+            return json.load(fp)
+        
+    def Generator(self): # Gera os objetos cliente.
+        self.clientes = []
+        for i in range (self.num):
+            self.clientes.append("cliente"+str(i))
+            self.clientes[i] = mqtt.Client(str(i))
+        return self.clientes
+    
+            
+    
+    def Rep(self): # Realiza o envio dos estados dos religadores
+        a = -1
+        while True:
+            for _ in self.clientes:
+                a += 1
+                self.myObj = self.getJSON()
+                #self.recloser = self.myObj["Recloser"]
+                obj = self.myObj[a]
+                _.connect("127.0.0.1")
+                _.loop_start()
+                _.publish("Topic_"+str(a),"Message:"+str(obj)+" From Client "+str(a))
+                _.loop_stop()
+            a = -1
+            time.sleep(10)
+        
+Teste = Reclosers()
+methods = [Teste.__init__(), Teste.createJSON(), Teste.getJSON(), Teste.Generator(),Teste.Rep()]
+for _ in methods:
+    _
+
             
