e7e9b17bd6b60514de727641e142148c2691dd42
Gabriel Oliveira Aragão <31046857+linknasubi@users.noreply.github.com>
2019-08-22 18:37:24 -0300

    Update Mqtt.py

--- Mqtt.py
+++ Mqtt.py
@@ -1,55 +1,66 @@
 import paho.mqtt.client as mqtt
 import time
 import json
+#from Listener import createJSON, Rep
 
-class Reclosers:
-    
+num = 18
+global pyld
+pyld = "Chave/0/Client:0"
+def on_message(client, userdata, message): #Callback para ouvir a mensagem que chega ao tópico enviado pelo servidor flask.
+    global pyld
+    pyld = "Chave/0/Client:0"
+    pyld = str((message.payload.decode('UTF-8')))
+    print(pyld)
 
-#    Reclosers que estão enviando seus estados a partir da leitura de um arquivo json.
-#    Estes devem armazenar seus estados atuais em um arquivo json gerado pelo próprio código. 
-    
-    def __init__(self):
-        self.num = 18
-        
-    def createJSON(self):
-        with open ("Teste4.json", "w") as outfile:
-            List = []
-            for _ in range (self.num):
-                Recloser = ([{"Id": str(_), "Chave": False, "Voltage": 0, "state": False, "waiting_state_change": False, "waiting_voltage_change": False}])
-                List.append(Recloser)
-            json.dump(List, outfile)
 
-    def getJSON(self):
-        with open("Teste4.json", 'r') as fp:
-            return json.load(fp)
-        
-    def Generator(self): # Gera os objetos cliente.
-        self.clientes = []
-        for i in range (self.num):
-            self.clientes.append("cliente"+str(i))
-            self.clientes[i] = mqtt.Client(str(i))
-        return self.clientes
+def createJSON(): #Criação do arquivo json a ser utilizado.
+    with open ("JsonClient.json", "w") as outfile:
+        List = []
+        for _ in range (num):
+            Recloser = ({"Id": str(_), "Chave": False, "Voltage": 0, "state": False, "waiting_state_change": False, "waiting_voltage_change": False})
+            List.append(Recloser)
+        json.dump(List, outfile)
+
+
+def getJSON(): #Leitura do arquivo json.
+    with open("JsonClient.json", 'r') as fp:
+        return json.load(fp)
     
-            
-    
-    def Rep(self): # Realiza o envio dos estados dos religadores
+def Generator(): # Gera os objetos cliente.
+    clientes = []
+    for i in range (num):
+        clientes.append("cliente"+str(i))
+        clientes[i] = mqtt.Client(str(i))
+        clientes[i].on_message = on_message
+    return clientes
+
+gen = Generator()
+
+def Rep(): # Escuta, Modifica, Lê e Publica o estado dos religadores respectivamente.
+    a = -1
+    while True:
+        for _ in gen:
+            #Tratamento das mensagens do Recloser
+            if pyld[-10]=="1":State = True
+            else: State = False
+            Sub = (len(pyld)) - 11 
+            Id = int(pyld[-1])
+            newpyld = pyld[0:(Sub)]
+            #-----------------------------------
+            a += 1
+            myObj = getJSON()
+            objL = myObj[Id]
+            objL[newpyld] = State
+            objP = myObj[a]     
+            with open("JsonClient.json", "w") as jsonFile:
+                json.dump(myObj, jsonFile)
+            _.connect("127.0.0.1", port = 1883)
+            _.loop_start()
+            _.subscribe("Changing", qos=2)
+            _.publish("Topic_"+str(a),"Message:"+str(objP)+" From Client "+str(a))
+            time.sleep(0.1)
+            _.loop_stop()
         a = -1
-        while True:
-            for _ in self.clientes:
-                a += 1
-                self.myObj = self.getJSON()
-                #self.recloser = self.myObj["Recloser"]
-                obj = self.myObj[a]
-                _.connect("127.0.0.1")
-                _.loop_start()
-                _.publish("Topic_"+str(a),"Message:"+str(obj)+" From Client "+str(a))
-                _.loop_stop()
-            a = -1
-            time.sleep(10)
-        
-Teste = Reclosers()
-methods = [Teste.__init__(), Teste.createJSON(), Teste.getJSON(), Teste.Generator(),Teste.Rep()]
-for _ in methods:
-    _
-
-            
+    
+createJSON()
+Rep()
